24b04ddb0395c1c23a46ac4f63c66404e3f51f1a
dbelinov <ddbelinov@gmail.com>
2024-11-14 16:44:03 +0200

    Added School Model

---
+++ SchoolManagementSystem/SchoolManagementSystem.Data.Models/School.cs
@@ -0,0 +1,11 @@
+namespace SchoolManagementSystem.Data.Models;
+
+public class School
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Address { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public ICollection<Class> Classes { get; set; } = new List<Class>();
+    public ICollection<Teacher> Teachers { get; set; } = new List<Teacher>();
+}
