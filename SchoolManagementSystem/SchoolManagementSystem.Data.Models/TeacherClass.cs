10495806e60edd28945668be089bf27685e3cbc6
dbelinov <ddbelinov@gmail.com>
2024-11-14 17:06:08 +0200

    Create TeacherClass Model

---
+++ SchoolManagementSystem/SchoolManagementSystem.Data.Models/TeacherClass.cs
@@ -0,0 +1,9 @@
+namespace SchoolManagementSystem.Data.Models;
+
+public class TeacherClass
+{
+    public int TeacherId { get; set; }
+    public Teacher Teacher { get; set; }
+    public int ClassId { get; set; }
+    public Class Class { get; set; }
+}
