50437e44ed9e96429d4f0155072735b24635fb3a
dbelinov <ddbelinov@gmail.com>
2024-11-20 19:19:38 +0200

    Update Teacher model

---
+++ SchoolManagementSystem/SchoolManagementSystem.Common/EntityConstants/TeacherConstants.cs
@@ -0,0 +1,13 @@
+namespace SchoolManagementSystem.Common.EntityConstants;
+
+public static class TeacherConstants
+{
+    public const int FirstNameMinLength = 2;
+    public const int FirstNameMaxLength = 10;
+    public const int MiddleNameMinLength = 2;
+    public const int MiddleNameMaxLength = 10;
+    public const int LastNameMinLength = 2;
+    public const int LastNameMaxLength = 20;
+    public const int SubjectMinValue = 1;
+    public const int SubjectMaxValue = 14;
+}
--- SchoolManagementSystem/SchoolManagementSystem.Data.Models/Teacher.cs
+++ SchoolManagementSystem/SchoolManagementSystem.Data.Models/Teacher.cs
@@ -1,11 +1,35 @@
+using System.ComponentModel.DataAnnotations;
 using SchoolManagementSystem.Common.Enums;
+
+using static SchoolManagementSystem.Common.EntityConstants.TeacherConstants;
 
 namespace SchoolManagementSystem.Data.Models;
 
 public class Teacher
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
+    public Teacher()
+    {
+        Id = Guid.NewGuid();
+        TeachersClasses = new List<TeacherClass>();
+    }
+    
+    [Key]
+    public Guid Id { get; set; }
+    
+    [Required]
+    [MaxLength(FirstNameMaxLength)]
+    public string FirstName { get; set; }
+    
+    [Required]
+    [MaxLength(MiddleNameMaxLength)]
+    public string MiddleName { get; set; }
+    
+    [Required]
+    [MaxLength(LastNameMaxLength)]
+    public string LastName { get; set; }
+    
+    [Required]
     public Subject Subject { get; set; }
-    public ICollection<TeacherClass> TeachersClasses { get; set; } =  new List<TeacherClass>();
+    
+    public ICollection<TeacherClass> TeachersClasses { get; set; }
 }
