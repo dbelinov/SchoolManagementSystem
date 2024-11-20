2e32f01e81c5abe04c3a3369162a10b6ddb447d2
dbelinov <ddbelinov@gmail.com>
2024-11-20 18:57:57 +0200

    Update School Model

---
+++ SchoolManagementSystem/SchoolManagementSystem.Common/EntityConstants/SchoolConstants.cs
@@ -0,0 +1,11 @@
+namespace SchoolManagementSystem.Common.EntityConstants;
+
+public static class SchoolConstants
+{
+    public const int NameMinLength = 2;
+    public const int NameMaxLength = 100;
+    public const int AddressMinLength = 10;
+    public const int AddressMaxLength = 150;
+    public const int DescriptionMinLength = 10;
+    public const int DescriptionMaxLength = 500;
+}
--- SchoolManagementSystem/SchoolManagementSystem.Data.Models/School.cs
+++ SchoolManagementSystem/SchoolManagementSystem.Data.Models/School.cs
@@ -1,11 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+using static SchoolManagementSystem.Common.EntityConstants.SchoolConstants;
+
 namespace SchoolManagementSystem.Data.Models;
 
 public class School
 {
+    [Required]
     public int Id { get; set; }
+    
+    [Required]
+    [MaxLength(NameMaxLength)]
     public string Name { get; set; } = null!;
+    
+    [Required]
+    [MaxLength(AddressMaxLength)]
     public string Address { get; set; } = null!;
-    public string Description { get; set; } = null!;
+    
+    [MaxLength(DescriptionMaxLength)]
+    public string? Description { get; set; }
+    
     public ICollection<Class> Classes { get; set; } = new List<Class>();
     public ICollection<Teacher> Teachers { get; set; } = new List<Teacher>();
     public ICollection<SchoolProject> SchoolsProjects { get; set; } = new List<SchoolProject>();
