ff5fdf2bb7d98286b371fab99dc5f6d5bdbce2fb
dbelinov <ddbelinov@gmail.com>
2024-11-20 18:58:38 +0200

    Update SchoolProject model

--- SchoolManagementSystem/SchoolManagementSystem.Data.Models/SchoolProject.cs
+++ SchoolManagementSystem/SchoolManagementSystem.Data.Models/SchoolProject.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace SchoolManagementSystem.Data.Models;
 
 public class SchoolProject
 {
+    [Required]
     public int SchoolId { get; set; }
+    [ForeignKey(nameof(SchoolId))]
     public School School { get; set; }
+    
+    [Required]
     public int ProjectId { get; set; }
+    [ForeignKey(nameof(ProjectId))]
     public Project Project { get; set; }
 }
