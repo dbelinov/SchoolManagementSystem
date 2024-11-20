f7f174744952c3f7c0ba8a5dae35b68502b61419
dbelinov <ddbelinov@gmail.com>
2024-11-20 19:13:54 +0200

    Update StudentGrade model

---
+++ SchoolManagementSystem/SchoolManagementSystem.Common/EntityConstants/StudentGradeConstants.cs
@@ -0,0 +1,9 @@
+namespace SchoolManagementSystem.Common.EntityConstants;
+
+public static class StudentGradeConstants
+{
+    public const int SubjectMinValue = 1;
+    public const int SubjectMaxValue = 14;
+    public const int GradeMinValue = 2;
+    public const int GradeMaxValue = 6;
+}
--- SchoolManagementSystem/SchoolManagementSystem.Data.Models/StudentGrade.cs
+++ SchoolManagementSystem/SchoolManagementSystem.Data.Models/StudentGrade.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
 using SchoolManagementSystem.Common.Enums;
 
 namespace SchoolManagementSystem.Data.Models;
 
 public class StudentGrade
 {
+    [Key]
+    public int Id { get; set; }
+    
+    [Required]
     public int StudentId { get; set; }
+    
+    [Required]
     public Subject Subject { get; set; }
+    
+    [Required]
     public int Grade { get; set; }
 }
