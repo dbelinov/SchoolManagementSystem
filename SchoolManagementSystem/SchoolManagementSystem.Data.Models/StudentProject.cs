285bf01d0c5dbb83ad51f9c1cd46517272affdcd
dbelinov <ddbelinov@gmail.com>
2024-11-20 19:15:04 +0200

    Update StudentProject model

--- SchoolManagementSystem/SchoolManagementSystem.Data.Models/StudentProject.cs
+++ SchoolManagementSystem/SchoolManagementSystem.Data.Models/StudentProject.cs
@@ -1,9 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
 namespace SchoolManagementSystem.Data.Models;
 
+[PrimaryKey(nameof(StudentId), nameof(ProjectId))]
 public class StudentProject
 {
+    [Required]
     public int StudentId { get; set; }
-    public Student Student { get; set; }
+    [ForeignKey(nameof(StudentId))]
+    public Student Student { get; set; } = null!;
+    
+    [Required]
     public int ProjectId { get; set; }
-    public Project Project { get; set; }
+    [ForeignKey(nameof(ProjectId))]
+    public Project Project { get; set; } = null!;
 }
