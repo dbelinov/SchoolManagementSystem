7eafbb835118c38c91796ffa184331c9a1384526
dbelinov <ddbelinov@gmail.com>
2024-11-20 19:21:35 +0200

    Update Timetable model

--- SchoolManagementSystem/SchoolManagementSystem.Data.Models/Timetable.cs
+++ SchoolManagementSystem/SchoolManagementSystem.Data.Models/Timetable.cs
@@ -1,8 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace SchoolManagementSystem.Data.Models;
 
 public class Timetable
 {
+    [Key]
     public int Id { get; set; }
+    
+    [Required]
     public int Period { get; set; }
-    public int Class { get; set; }
+    
+    [Required]
+    public int ClassId { get; set; }
+    [ForeignKey(nameof(ClassId))]
+    public Class Class { get; set; } = null!;
 }
