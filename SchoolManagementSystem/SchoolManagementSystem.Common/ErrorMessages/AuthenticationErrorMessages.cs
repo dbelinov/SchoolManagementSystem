4a6b88c59d91d9e720fa58186c2898fcb8e9c3c0
dbelinov <ddbelinov@gmail.com>
2024-11-23 16:03:37 +0200

    Add more properties to the register

--- SchoolManagementSystem/SchoolManagementSystem.Common/ErrorMessages/AuthenticationErrorMessages.cs
+++ SchoolManagementSystem/SchoolManagementSystem.Common/ErrorMessages/AuthenticationErrorMessages.cs
@@ -8,4 +8,15 @@ public static class AuthenticationErrorMessages
     public const string PasswordLengthError = "Password must be between 6 and 100 characters long!";
     public const string PasswordMatchError = "Password don't match!";
     public const string IdNumberInvalid = "Invalid id!";
+    public const string InvalidFirstName = "First name should be between 2 and 10 characters!";
+    public const string InvalidMiddleName = "First name should be between 2 and 10 characters!";
+    public const string InvalidLastName = "First name should be between 2 and 20 characters!";
+    public const string EmailRequired = "Email is required!";
+    public const string PasswordRequired = "Password is required!";
+    public const string ConfirmPasswordRequired = "Confirm password is required!";
+    public const string IdNumberRequired = "Identification Number is required!";
+    public const string FirstNameRequired = "First name is required!";
+    public const string MiddleNameRequired = "Middle name is required!";
+    public const string LastNameRequired = "Last name is required!";
+    public const string BirthDateRequired = "Birth date is required!";
 }
--- SchoolManagementSystem/SchoolManagementSystem.Web.ViewModels/RegisterFormViewModel.cs
+++ SchoolManagementSystem/SchoolManagementSystem.Web.ViewModels/RegisterFormViewModel.cs
@@ -7,22 +7,38 @@ namespace SchoolManagementSystem.Web.ViewModels;
 
 public class RegisterFormViewModel
 {
-    [Required]
+    [Required(ErrorMessage = EmailRequired)]
     [EmailAddress]
-    public string Email { get; set; }
+    public string Email { get; set; } = string.Empty;
     
-    [Required]
+    [Required(ErrorMessage = PasswordRequired)]
     [StringLength(PasswordMaxLength, MinimumLength = PasswordMinLength, 
         ErrorMessage = PasswordLengthError)]
     [DataType(DataType.Password)]
-    public string Password { get; set; }
+    public string Password { get; set; } = string.Empty;
     
-    [Required]
+    [Required(ErrorMessage = ConfirmPasswordRequired)]
     [DataType(DataType.Password)]
     [Compare("Password", ErrorMessage = PasswordMatchError)]
-    public string ConfirmPassword { get; set; }
+    public string ConfirmPassword { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = FirstNameRequired)]
+    [StringLength(FirstNameMaxLength, MinimumLength = FirstNameMinLength, ErrorMessage = InvalidFirstName)]
+    public string FirstName { get; set; } = string.Empty;
     
-    [Required]
+    [Required(ErrorMessage = MiddleNameRequired)]
+    [StringLength(MiddleNameMaxLength, MinimumLength = MiddleNameMinLength, ErrorMessage = InvalidMiddleName)]
+    public string MiddleName { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = LastNameRequired)]
+    [StringLength(LastNameMaxLength, MinimumLength = LastNameMinLength, ErrorMessage = InvalidLastName)]
+    public string LastName { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = BirthDateRequired)]
+    [DataType(DataType.DateTime)]
+    public DateTime BirthDate { get; set; }
+    
+    [Required(ErrorMessage = IdNumberRequired)]
     [StringLength(IdNumberLength, MinimumLength = IdNumberLength, ErrorMessage = IdNumberInvalid)]
-    public string IdNumber { get; set; }
+    public string IdNumber { get; set; } = string.Empty;
 }
