245558a5bd848dc8d5b2040d66813faac65881e3
dbelinov <ddbelinov@gmail.com>
2024-12-06 19:12:56 +0200

    Add Grade and Remove Grade added for teachers

--- SchoolManagementSystem/SchoolManagementSystem.Web.ViewModels/TeacherGradesViewModel.cs
+++ SchoolManagementSystem/SchoolManagementSystem.Web.ViewModels/TeacherGradesViewModel.cs
@@ -4,6 +4,7 @@ namespace SchoolManagementSystem.Web.ViewModels;
 
 public class TeacherGradesViewModel
 {
+    public Guid StudentId { get; set; }
     public string StudentName { get; set; } = string.Empty;
     public ICollection<Grade> Grades { get; set; } = new List<Grade>(); 
 }
--- SchoolManagementSystem/SchoolManagementSystem.Web/Controllers/TeacherController.cs
+++ SchoolManagementSystem/SchoolManagementSystem.Web/Controllers/TeacherController.cs
@@ -92,8 +92,7 @@ public class TeacherController : Controller
 
         if (user is null)
         {
-            ModelState.AddModelError("user", NotLoggedIn);
-            return RedirectToAction("Index", "Home");
+            return BadRequest();
         }
         
         var teacher = await _context.Teachers
@@ -103,8 +102,7 @@ public class TeacherController : Controller
 
         if (teacher is null)
         {
-            ModelState.AddModelError("user", NotAuthenticated);
-            return RedirectToAction("Index", "Home");
+            return BadRequest();
         }
 
         var studentsInClass = await _context.Students
@@ -115,6 +113,7 @@ public class TeacherController : Controller
         var models = studentsInClass
             .Select(student => new TeacherGradesViewModel
             {
+                StudentId = student.Id,
                 StudentName = $"{student.FirstName} {student.MiddleName} {student.LastName}",
                 Grades = student.Grades
                     .Where(g => g.Subject == teacher.Subject)
@@ -123,5 +122,75 @@ public class TeacherController : Controller
             .ToList();
 
         return View(models);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddGrade(int gradeValue, Guid studentId)
+    {
+        var user = await _userManager.GetUserAsync(HttpContext.User);
+
+        if (user is null)
+        {
+            return BadRequest();
+        }
+        
+        var teacher = await _context.Teachers
+            .FirstOrDefaultAsync(t => t.IdNumber == user.IdNumber);
+        
+        var student = await _context.Students
+            .FirstOrDefaultAsync(s => s.Id == studentId);
+
+        if (teacher is null || student is null)
+        {
+            return BadRequest();
+        }
+
+        Grade grade = new Grade()
+        {
+            StudentId = student.Id,
+            TeacherId = teacher.Id,
+            Subject = teacher.Subject,
+            GradeValue = gradeValue,
+        };
+
+        var classId = student.ClassId;
+        
+        student.Grades.Add(grade);
+        _context.Update(student);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction("ClassGrades", "Teacher", new { classId });
+    }
+
+    public async Task<IActionResult> DeleteGrade(int gradeId, Guid studentId)
+    {
+        var grade = await _context.Grades.FirstOrDefaultAsync(g => g.Id == gradeId);
+        var student = await _context.Students.FirstOrDefaultAsync(s => s.Id == studentId);
+        
+        var user  = await _userManager.GetUserAsync(HttpContext.User);
+        if (user is null)
+        {
+            return BadRequest();
+        }        
+        
+        var teacher = await _context.Teachers
+            .FirstOrDefaultAsync(t => t.Id == user.AppId);
+
+        if (grade is null || student is null || teacher is null)
+        {
+            return BadRequest();
+        }
+        
+        var classId = student.ClassId;
+
+        if (grade.TeacherId == teacher.Id)
+        {
+            student.Grades.Remove(grade);
+            _context.Update(student);
+            _context.Remove(grade);
+            await _context.SaveChangesAsync();
+        }
+        
+        return RedirectToAction("ClassGrades", "Teacher", new { classId });
     }
 }
