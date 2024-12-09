9bab6918c6173409cecec169c24956fca5b59c3e
dbelinov <ddbelinov@gmail.com>
2024-12-09 22:50:13 +0200

    Create student scoreboard for teachers

--- SchoolManagementSystem/SchoolManagementSystem.Web/Controllers/TeacherController.cs
+++ SchoolManagementSystem/SchoolManagementSystem.Web/Controllers/TeacherController.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SchoolManagementSystem.Data;
 using SchoolManagementSystem.Data.Models;
 using SchoolManagementSystem.Data.Models.IdentityModels;
 using SchoolManagementSystem.Services.Contracts;
+using SchoolManagementSystem.Web.ViewModels;
 
 namespace SchoolManagementSystem.Web.Controllers;
 
@@ -13,11 +16,13 @@ public class TeacherController : Controller
 {
     private readonly ITeacherService _teacherService;
     private readonly UserManager<ApplicationUser> _userManager;
+    private readonly ApplicationDbContext _context;
 
-    public TeacherController(ITeacherService teacherService, UserManager<ApplicationUser> userManager)
+    public TeacherController(ITeacherService teacherService, UserManager<ApplicationUser> userManager, ApplicationDbContext context)
     {
         _teacherService = teacherService;
         _userManager = userManager;
+        _context = context;
     }
 
     [HttpGet]
@@ -103,5 +108,66 @@ public class TeacherController : Controller
         }
 
         return RedirectToAction("ClassGrades", "Teacher", new { classId = success });
+    }
+
+    public async Task<IActionResult> Scoreboard()
+    {
+        var user = await _userManager.GetUserAsync(User);
+
+        if (user == null)
+        {
+            return BadRequest();
+        }
+
+        var teacher = await _context.Teachers
+            .Include(t => t.TeachersClasses)
+            .ThenInclude(tc => tc.Class)
+            .ThenInclude(c => c.Students)
+            .ThenInclude(student => student.Grades)
+            .FirstOrDefaultAsync(t => t.IdNumber == user.IdNumber);
+
+        if (teacher == null)
+        {
+            return BadRequest();
+        }
+
+        var classes = teacher.TeachersClasses.Select(c => c.Class).ToList();
+
+        var students = classes
+            .SelectMany(c => c.Students)
+            .Distinct()
+            .ToList();
+
+        var studentScoreboard = students
+            .Select(s => new StudentScoreboardViewModel
+            {
+                FullName = $"{s.FirstName} {s.MiddleName} {s.LastName}",
+                ClassName = s.Class.Name,
+                AverageGrade = s.Grades.Any(g => g.Subject == teacher.Subject) ? s.Grades
+                    .Average(g => g.GradeValue) : 0 
+            })
+            .ToList();
+
+
+        var classesScoreboard = classes
+            .Select(c => new ClassScoreboardViewModel
+            {
+                ClassName = c.Name,
+                AverageGrade = c.Students
+                    .Where(s => s.Grades
+                        .Any(g => g.Subject == teacher.Subject))
+                    .Select(s => s.Grades.Average(g => g.GradeValue))
+                    .DefaultIfEmpty(0)
+                    .Average()
+            })
+            .ToList();
+
+        var model = new ScoreboardViewModel
+        {
+            ClassScores = classesScoreboard,
+            StudentScores = studentScoreboard,
+        };
+        
+        return View(model);
     }
 }
